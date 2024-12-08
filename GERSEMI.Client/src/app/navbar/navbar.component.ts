a1f8978b4e17ef48878317e05941adfd0d142c18
MAJDER0 <tomasz.majder89@gmail.com>
2024-12-08 21:38:52 +0100

    sub-nav appearance

--- GERSEMI.Client/src/app/navbar/navbar.component.ts
+++ GERSEMI.Client/src/app/navbar/navbar.component.ts
@@ -9,5 +9,31 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  SubNavState: string = 'hidden';
+  isMouseOverSklepButton: boolean = false;
+  leftButtonArea : boolean = false;
 
+  leftButton(left:boolean): void {
+    this.leftButtonArea = left;
+  }
+
+  ShowSubNav(state: string): void {
+    if (!this.isMouseOverSklepButton || this.leftButtonArea == true) {
+      this.SubNavState = state;
+    }
+  }
+
+  toggleMouseOverSklepButton(enter: boolean): void {
+    this.isMouseOverSklepButton = enter;
+    console.log("State: ",this.SubNavState," isMouseOverSklepButton: ", this.isMouseOverSklepButton,"LeftButtonArea: ", this.leftButtonArea);
+  }
+
+  hideSubNavOnMouseLeave(event: MouseEvent): void {
+    const target = event.relatedTarget as HTMLElement;
+    // Check if the mouse leaves the Sklep button or subNav
+    if (!target || (!target.classList.contains('sklep-button') && !target.closest('#subNav'))) {
+      this.SubNavState = 'hidden';
+      this.isMouseOverSklepButton = false;
+    }
+  }
 }
