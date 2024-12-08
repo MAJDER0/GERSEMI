7c7b34f4ab6afa8758eb1c74bd00fe9025d98fd6
MAJDER0 <tomasz.majder89@gmail.com>
2024-12-09 00:29:02 +0100

    Responsive footer

--- GERSEMI.Client/tailwind.config.js
+++ GERSEMI.Client/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'custom950': '950px',
         'custom868': '868px',
         'custom1168': '1168px',
+        'custom428': '428px',
+        'custom376': '376px',
       },
     },
   },
