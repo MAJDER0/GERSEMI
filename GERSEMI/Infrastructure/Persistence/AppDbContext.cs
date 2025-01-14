8144e1cd3e7966b53092a066c71a885a7949c88f
MAJDER0 <tomasz.majder89@gmail.com>
2025-01-14 22:42:56 +0100

    Infrastructure Configurations

---
+++ GERSEMI/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductPrice> ProductPrices => Set<ProductPrice>();
+        public DbSet<ProductVariant> ProductVariants => Set<ProductVariant>();
+        public DbSet<ProductTranslation> ProductTranslations => Set<ProductTranslation>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<CategoryTranslation> CategoryTranslations => Set<CategoryTranslation>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Store> Stores => Set<Store>();
+        public DbSet<Coupon> Coupons => Set<Coupon>();
+        public DbSet<CouponUsage> CouponUsages => Set<CouponUsage>();
+        public DbSet<Employee> Employees => Set<Employee>();
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new CouponConfiguration());
+            modelBuilder.ApplyConfiguration(new CouponUsageConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductPriceConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductVariantConfiguration());
+            modelBuilder.ApplyConfiguration(new StoreConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
